5f8ed9a622b157b16451f63771174f0b5ea76131
emirmorgan <53477261+emirmorgan@users.noreply.github.com>
2023-07-01 06:11:06 +0300

    feat: HomeScrollable is now draggable

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,16 @@ export default function Home() {
     <main className="min-h-screen">
       <Navbar />
       <div className="container mx-auto">
-        <HomeScrollable />
+        <HomeScrollable title="Example cards">
+          {Array.from({ length: 20 }, (_, i) => (
+            <div
+              key={i}
+              className="flex items-center justify-center text-gray-500 border border-dashed w-44 h-72 shrink-0 bg-neutral-100 border-negative-300"
+            >
+              {i + 1}
+            </div>
+          ))}
+        </HomeScrollable>
       </div>
     </main>
   );
