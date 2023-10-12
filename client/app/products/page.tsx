7f36dfcbcf863eeab1065a720d9fcbf6f6c63a37
emirmorgan <53477261+emirmorgan@users.noreply.github.com>
2023-10-12 06:17:43 +0300

    feat: MobileFilterTab

--- client/app/products/page.tsx
+++ client/app/products/page.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { AiFillFilter } from "react-icons/ai";
+import { useState } from "react";
+
 import { useProducts } from "@/context/ProductsContext";
 import { useURLParams } from "@/context/ParamsContext";
 
@@ -8,19 +11,27 @@ import ProductCard from "@/components/ProductCard";
 import FilterTab from "@/components/Products/FilterTab";
 import Sort from "@/components/Products/Sort";
 import ProductCardSkeleton from "@/components/ProductCard/ProductCardSkeleton";
+import MobileFilterTab from "@/components/Products/MobileFilterTab";
 
 export default function ProductsPage() {
   const { q } = useURLParams();
   const { productsResponse, isLoading } = useProducts();
 
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="flex justify-center mx-auto gap-3 my-3">
-      <FilterTab />
+      <div className="hidden md:flex h-fit flex-col min-w-[250px] max-w-[250px] border border-gray-300">
+        <div className="p-3">
+          <span className="font-semibold">Filters</span>
+        </div>
+        <FilterTab />
+      </div>
       <div className="flex flex-1 flex-col">
         {q && (
           <span className="font-semibold text-xl">{`"${q}" search shows ${productsResponse.totalProducts} results`}</span>
         )}
-        <div className="flex justify-between items-center">
+        <div className="flex flex-col justify-between items-center gap-2 sm:gap-0 sm:flex-row">
           <span className="font-semibold">
             Showing {productsResponse.pageNumber * 1} -{" "}
             {productsResponse.pageNumber * productsResponse.pageSize} of{" "}
@@ -48,6 +59,14 @@ export default function ProductsPage() {
                 />
               ))}
         </div>
+      </div>
+      <MobileFilterTab isOpen={isOpen} setIsOpen={setIsOpen} />
+      <div
+        onClick={() => setIsOpen(true)}
+        className="fixed z-[10] bottom-[75px] flex justify-center items-center border-2 border-gray-400 bg-white text-black font-semibold select-none cursor-pointer px-4 py-2 gap-2 transition-all ease-linear hover:border-black md:hidden"
+      >
+        <AiFillFilter />
+        <span>Filter</span>
       </div>
     </div>
   );
--- client/components/Products/FilterTab.tsx
+++ client/components/Products/FilterTab.tsx
@@ -19,7 +19,7 @@ export default function FilterTab() {
     createQueryString,
   } = useURLParams();
 
-  const [toggleCategory, setToggleCategory] = useState(false);
+  const [toggleCategory, setToggleCategory] = useState(true);
   const [toggleSubcategory, setToggleSubcategory] = useState(false);
   const [toggleBrand, setToggleBrand] = useState(false);
   const [toggleColor, setToggleColor] = useState(false);
@@ -49,38 +49,109 @@ export default function FilterTab() {
   };
 
   return (
-    <div className="flex flex-col border border-gray-300 min-w-[250px] max-w-[250px] h-fit select-none">
-      <div className="p-3">
-        <span className="font-semibold">Filters</span>
+    <div className="flex flex-col h-full select-none gap-2">
+      <div className="flex flex-col border-y cursor-pointer">
+        <div
+          onClick={() => setToggleCategory(!toggleCategory)}
+          className="flex w-full justify-between items-center p-2"
+        >
+          <div className="flex flex-col">
+            <span className="text-gray-600 text-sm">Category</span>
+            <span>{currentCategory ? currentCategory : "Any"}</span>
+          </div>
+          <AiFillCaretDown
+            className={
+              "transition-all ease-linear duration-[250ms] " +
+              (toggleCategory ? " -rotate-180" : "")
+            }
+            size={20}
+          />
+        </div>
+        <ul
+          className={
+            " flex flex-col transition-all ease-linear overflow-auto scrollbar-cart" +
+            (toggleCategory ? "  h-[150px]" : "  h-[0px]")
+          }
+        >
+          <li
+            onClick={() =>
+              router.push(
+                pathname + "?" + createQueryString("category", "Any"),
+                {
+                  scroll: false,
+                }
+              )
+            }
+            className="flex items-center border-t border-gray-200 p-1 hover:bg-black/5"
+          >
+            <div className="w-[16px] h-[2px] bg-gray-500 mx-2" />
+            Any
+          </li>
+          {categories.map((category, index) => (
+            <li
+              key={index}
+              onClick={() =>
+                router.push(
+                  pathname +
+                    "?" +
+                    createQueryString("category", category as string),
+                  {
+                    scroll: false,
+                  }
+                )
+              }
+              className="flex items-center border-t border-gray-200 p-1 hover:bg-black/5"
+            >
+              <div className="w-[16px] h-[2px] bg-gray-500 mx-2" /> {category}
+            </li>
+          ))}
+        </ul>
       </div>
-      <div className="flex flex-col">
-        <div className="flex flex-col border-t cursor-pointer">
-          <div
-            onClick={() => setToggleCategory(!toggleCategory)}
-            className="flex w-full justify-between items-center p-2"
-          >
-            <div className="flex flex-col">
-              <span className="text-gray-600 text-sm">Category</span>
-              <span>{currentCategory ? currentCategory : "Any"}</span>
-            </div>
-            <AiFillCaretDown
-              className={
-                "transition-all ease-linear duration-[250ms] " +
-                (toggleCategory ? " -rotate-180" : "")
-              }
-              size={20}
-            />
+      <div className="flex flex-col border-y cursor-pointer">
+        <div
+          onClick={() => setToggleSubcategory(!toggleSubcategory)}
+          className="flex w-full justify-between items-center p-2"
+        >
+          <div className="flex flex-col">
+            <span className="text-gray-600 text-sm">Subcategory</span>
+            <span>{currentSubcategory ? currentSubcategory : "Any"}</span>
           </div>
-          <ul
+          <AiFillCaretDown
             className={
-              " flex flex-col transition-all ease-linear overflow-auto scrollbar-cart" +
-              (toggleCategory ? "  h-[150px]" : "  h-[0px]")
+              "transition-all ease-linear duration-[250ms] " +
+              (toggleSubcategory ? " -rotate-180" : "")
             }
+            size={20}
+          />
+        </div>
+        <ul
+          className={
+            " flex flex-col transition-all ease-linear overflow-auto scrollbar-cart" +
+            (toggleSubcategory ? "  h-[150px]" : "  h-[0px]")
+          }
+        >
+          <li
+            onClick={() =>
+              router.push(
+                pathname + "?" + createQueryString("subcategory", "Any"),
+                {
+                  scroll: false,
+                }
+              )
+            }
+            className="flex items-center border-t border-gray-200 p-1 hover:bg-black/5"
           >
+            <div className="w-[16px] h-[2px] bg-gray-500 mx-2" />
+            Any
+          </li>
+          {subcategories.map((subcategory, index) => (
             <li
+              key={index}
               onClick={() =>
                 router.push(
-                  pathname + "?" + createQueryString("category", "Any"),
+                  pathname +
+                    "?" +
+                    createQueryString("subcategory", subcategory),
                   {
                     scroll: false,
                   }
@@ -89,55 +160,51 @@ export default function FilterTab() {
               className="flex items-center border-t border-gray-200 p-1 hover:bg-black/5"
             >
               <div className="w-[16px] h-[2px] bg-gray-500 mx-2" />
-              Any
+              {subcategory}
             </li>
-            {categories.map((category, index) => (
-              <li
-                key={index}
-                onClick={() =>
-                  router.push(
-                    pathname +
-                      "?" +
-                      createQueryString("category", category as string),
-                    {
-                      scroll: false,
-                    }
-                  )
-                }
-                className="flex items-center border-t border-gray-200 p-1 hover:bg-black/5"
-              >
-                <div className="w-[16px] h-[2px] bg-gray-500 mx-2" /> {category}
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="flex flex-col border-t cursor-pointer">
-          <div
-            onClick={() => setToggleSubcategory(!toggleSubcategory)}
-            className="flex w-full justify-between items-center p-2"
-          >
-            <div className="flex flex-col">
-              <span className="text-gray-600 text-sm">Subcategory</span>
-              <span>{currentSubcategory ? currentSubcategory : "Any"}</span>
-            </div>
-            <AiFillCaretDown
-              className={
-                "transition-all ease-linear duration-[250ms] " +
-                (toggleSubcategory ? " -rotate-180" : "")
-              }
-              size={20}
-            />
+          ))}
+        </ul>
+      </div>
+      <div className="flex flex-col border-y cursor-pointer">
+        <div
+          onClick={() => setToggleBrand(!toggleBrand)}
+          className="flex w-full justify-between items-center p-2"
+        >
+          <div className="flex flex-col">
+            <span className="text-gray-600 text-sm">Brands</span>
+            <span>{currentBrand ? currentBrand : "Any"}</span>
           </div>
-          <ul
+          <AiFillCaretDown
             className={
-              " flex flex-col transition-all ease-linear overflow-auto scrollbar-cart" +
-              (toggleSubcategory ? "  h-[150px]" : "  h-[0px]")
+              "transition-all ease-linear duration-[250ms] " +
+              (toggleBrand ? " -rotate-180" : "")
             }
+            size={20}
+          />
+        </div>
+        <ul
+          className={
+            " flex flex-col transition-all ease-linear overflow-auto scrollbar-cart" +
+            (toggleBrand ? "  h-[150px]" : "  h-[0px]")
+          }
+        >
+          <li
+            onClick={() =>
+              router.push(pathname + "?" + createQueryString("brand", "Any"), {
+                scroll: false,
+              })
+            }
+            className="flex items-center border-t border-gray-200 p-1 hover:bg-black/5"
           >
+            <div className="w-[16px] h-[2px] bg-gray-500 mx-2" />
+            Any
+          </li>
+          {brands.map((brand, index) => (
             <li
+              key={index}
               onClick={() =>
                 router.push(
-                  pathname + "?" + createQueryString("subcategory", "Any"),
+                  pathname + "?" + createQueryString("brand", brand),
                   {
                     scroll: false,
                   }
@@ -146,181 +213,109 @@ export default function FilterTab() {
               className="flex items-center border-t border-gray-200 p-1 hover:bg-black/5"
             >
               <div className="w-[16px] h-[2px] bg-gray-500 mx-2" />
-              Any
+              {brand}
             </li>
-            {subcategories.map((subcategory, index) => (
-              <li
-                key={index}
-                onClick={() =>
-                  router.push(
-                    pathname +
-                      "?" +
-                      createQueryString("subcategory", subcategory),
-                    {
-                      scroll: false,
-                    }
-                  )
-                }
-                className="flex items-center border-t border-gray-200 p-1 hover:bg-black/5"
-              >
-                <div className="w-[16px] h-[2px] bg-gray-500 mx-2" />
-                {subcategory}
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="flex flex-col border-t cursor-pointer">
-          <div
-            onClick={() => setToggleBrand(!toggleBrand)}
-            className="flex w-full justify-between items-center p-2"
-          >
-            <div className="flex flex-col">
-              <span className="text-gray-600 text-sm">Brands</span>
-              <span>{currentBrand ? currentBrand : "Any"}</span>
-            </div>
-            <AiFillCaretDown
-              className={
-                "transition-all ease-linear duration-[250ms] " +
-                (toggleBrand ? " -rotate-180" : "")
-              }
-              size={20}
-            />
-          </div>
-          <ul
-            className={
-              " flex flex-col transition-all ease-linear overflow-auto scrollbar-cart" +
-              (toggleBrand ? "  h-[150px]" : "  h-[0px]")
-            }
-          >
-            <li
-              onClick={() =>
-                router.push(
-                  pathname + "?" + createQueryString("brand", "Any"),
-                  {
-                    scroll: false,
-                  }
-                )
-              }
-              className="flex items-center border-t border-gray-200 p-1 hover:bg-black/5"
-            >
-              <div className="w-[16px] h-[2px] bg-gray-500 mx-2" />
-              Any
-            </li>
-            {brands.map((brand, index) => (
-              <li
-                key={index}
-                onClick={() =>
-                  router.push(
-                    pathname + "?" + createQueryString("brand", brand),
-                    {
-                      scroll: false,
-                    }
-                  )
-                }
-                className="flex items-center border-t border-gray-200 p-1 hover:bg-black/5"
-              >
-                <div className="w-[16px] h-[2px] bg-gray-500 mx-2" />
-                {brand}
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="flex flex-col border-t cursor-pointer">
-          <div
-            onClick={() => setToggleColor(!toggleColor)}
-            className="flex w-full justify-between items-center p-2"
-          >
-            <div className="flex flex-col">
-              <span className="text-gray-600 text-sm">Color</span>
-              <div className="flex items-center justify-center gap-2">
-                <div
-                  className="border w-[16px] h-[16px] rounded-full"
-                  style={{
-                    backgroundColor: colors.find(
-                      (color) => color.label == currentColor
-                    )?.hex,
-                  }}
-                />
-                <span>{currentColor ? currentColor : "Any"}</span>
-              </div>
-            </div>
-            <AiFillCaretDown
-              className={
-                "transition-all ease-linear duration-[250ms] " +
-                (toggleColor ? " -rotate-180" : "")
-              }
-              size={20}
-            />
-          </div>
-          <ul
-            className={
-              " flex flex-col transition-all ease-linear overflow-auto scrollbar-cart" +
-              (toggleColor ? "  h-[150px]" : "  h-[0px]")
-            }
-          >
-            <li
-              onClick={() =>
-                router.push(
-                  pathname + "?" + createQueryString("color", "Any"),
-                  {
-                    scroll: false,
-                  }
-                )
-              }
-              className="flex items-center border-t border-gray-200 p-1 hover:bg-black/5"
-            >
-              <div className="w-[16px] h-[2px] bg-gray-500 mx-2" />
-              Any
-            </li>
-            {colors.map((color, index) => (
-              <li
-                key={index}
-                onClick={() =>
-                  router.push(
-                    pathname + "?" + createQueryString("color", color.label),
-                    {
-                      scroll: false,
-                    }
-                  )
-                }
-                className="flex items-center border-t border-gray-200 p-1 hover:bg-black/5"
-              >
-                <div
-                  className="border w-[16px] h-[16px] rounded-full mx-2"
-                  style={{ backgroundColor: color.hex }}
-                />
-                {color.label}
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="flex flex-col border-t gap-1 p-2">
-          <span>Price</span>
-          <form onSubmit={handlePrice}>
-            <div className="flex justify-center gap-3">
-              <input
-                className="w-full border px-2 py-1 focus:border-black"
-                placeholder="From"
-                type="number"
-                name="priceFrom"
-                min={0}
+          ))}
+        </ul>
+      </div>
+      <div className="flex flex-col border-y cursor-pointer">
+        <div
+          onClick={() => setToggleColor(!toggleColor)}
+          className="flex w-full justify-between items-center p-2"
+        >
+          <div className="flex flex-col">
+            <span className="text-gray-600 text-sm">Color</span>
+            <div className="flex items-center justify-center gap-2">
+              <div
+                className="border w-[16px] h-[16px] rounded-full"
+                style={{
+                  backgroundColor: colors.find(
+                    (color) => color.label == currentColor
+                  )?.hex,
+                }}
               />
-              <input
-                className="w-full border px-2 py-1 focus:border-black"
-                placeholder="To"
-                type="number"
-                name="priceTo"
-                min={0}
-              />
+              <span>{currentColor ? currentColor : "Any"}</span>
             </div>
-            <button
-              type="submit"
-              className="flex items-center justify-center w-full border border-black bg-white text-black font-semibold cursor-pointer mt-1 p-1 transition-all ease-linear hover:bg-black hover:text-white"
-            >
-              Apply
-            </button>
-          </form>
+          </div>
+          <AiFillCaretDown
+            className={
+              "transition-all ease-linear duration-[250ms] " +
+              (toggleColor ? " -rotate-180" : "")
+            }
+            size={20}
+          />
         </div>
+        <ul
+          className={
+            " flex flex-col transition-all ease-linear overflow-auto scrollbar-cart" +
+            (toggleColor ? "  h-[150px]" : "  h-[0px]")
+          }
+        >
+          <li
+            onClick={() =>
+              router.push(pathname + "?" + createQueryString("color", "Any"), {
+                scroll: false,
+              })
+            }
+            className="flex items-center border-t border-gray-200 p-1 hover:bg-black/5"
+          >
+            <div className="w-[16px] h-[2px] bg-gray-500 mx-2" />
+            Any
+          </li>
+          {colors.map((color, index) => (
+            <li
+              key={index}
+              onClick={() =>
+                router.push(
+                  pathname + "?" + createQueryString("color", color.label),
+                  {
+                    scroll: false,
+                  }
+                )
+              }
+              className="flex items-center border-t border-gray-200 p-1 hover:bg-black/5"
+            >
+              <div
+                className="border w-[16px] h-[16px] rounded-full mx-2"
+                style={{ backgroundColor: color.hex }}
+              />
+              {color.label}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="flex flex-col border-y gap-1 p-2">
+        <span>Price</span>
+        <form onSubmit={handlePrice}>
+          <div className="flex justify-center gap-3">
+            <input
+              className="w-full border px-2 py-1 focus:border-black"
+              placeholder="From"
+              type="number"
+              name="priceFrom"
+              min={0}
+            />
+            <input
+              className="w-full border px-2 py-1 focus:border-black"
+              placeholder="To"
+              type="number"
+              name="priceTo"
+              min={0}
+            />
+          </div>
+          <button
+            type="submit"
+            className="flex items-center justify-center w-full border border-black bg-white text-black font-semibold cursor-pointer mt-1 p-1 transition-all ease-linear hover:bg-black hover:text-white"
+          >
+            Apply
+          </button>
+        </form>
+      </div>
+      <div
+        onClick={() => router.push("/products")}
+        className="flex justify-center items-center w-8/12 mx-auto bg-black text-white font-semibold cursor-pointer p-2 mb-2"
+      >
+        Clear
       </div>
     </div>
   );
