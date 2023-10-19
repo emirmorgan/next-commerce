6158a1731a79e69879fd265e0a89f33658eb5163
emirmorgan <53477261+emirmorgan@users.noreply.github.com>
2023-10-19 05:23:39 +0300

    feat(Dashboard/Main): better design

--- client/app/dashboard/page.tsx
+++ client/app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 import {
+  AiFillDollarCircle,
+  AiFillShopping,
   AiOutlineAppstore,
-  AiOutlineDollarCircle,
   AiOutlineShoppingCart,
   AiOutlineUser,
 } from "react-icons/ai";
@@ -14,28 +15,36 @@ export default function DashboardPage() {
       <h1 className="text-xl font-semibold mt-3">Dashboard</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 lg:flex-row gap-3 mt-4">
         <div className="flex items-center border rounded p-3">
-          <AiOutlineDollarCircle size={48} />
+          <div className="flex items-center justify-center bg-green-200 text-green-700 w-12 h-12 rounded-full">
+            <AiFillDollarCircle size={28} />
+          </div>
           <div className="flex flex-col font-semibold ml-4">
             <span className="text-sm text-gray-500">Total Sales</span>
             <span className="text-xl">34.855$</span>
           </div>
         </div>
         <div className="flex items-center border rounded p-3">
-          <AiOutlineShoppingCart size={48} />
+          <div className="flex items-center justify-center bg-blue-200 text-blue-700 w-12 h-12 rounded-full">
+            <AiFillShopping size={28} />
+          </div>
           <div className="flex flex-col font-semibold ml-4">
             <span className="text-sm text-gray-500">Total Orders</span>
             <span className="text-xl">211</span>
           </div>
         </div>
         <div className="flex items-center border rounded p-3">
-          <AiOutlineAppstore size={48} />
+          <div className="flex items-center justify-center bg-orange-200 text-orange-700 w-12 h-12 rounded-full">
+            <AiOutlineAppstore size={28} />
+          </div>
           <div className="flex flex-col font-semibold ml-4">
             <span className="text-sm text-gray-500">Total Products</span>
             <span className="text-xl">345</span>
           </div>
         </div>
         <div className="flex items-center border rounded p-3">
-          <AiOutlineUser size={48} />
+          <div className="flex items-center justify-center bg-purple-200 text-purple-700 w-12 h-12 rounded-full">
+            <AiOutlineUser size={28} />
+          </div>
           <div className="flex flex-col font-semibold ml-4">
             <span className="text-sm text-gray-500">Total Customers</span>
             <span className="text-xl">4551</span>
